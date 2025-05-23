33bb0e332a17cf95992921bc467611ecf40dcded
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-23 14:25:48 +0900

    feat: 로그인 구현

--- client/src/routers/LoginPage.jsx
+++ client/src/routers/LoginPage.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { login } from "@/api/user.js";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const username = e.target[0].value;
+    const password = e.target[1].value;
+
+    await login({ username, password });
+  };
+
+  return (
+    <>
+      <h1>로그인</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="아이디" />
+        <input type="password" placeholder="비밀번호" />
+        <button>로그인</button>
+      </form>
+    </>
+  );
 }
 
 export default LoginPage;
